e907717c1a04b9fdd5542687f7d0411777dbc8d7
Palathip <windeesel365@gmail.com>
2023-09-19 19:31:39 +0700

    feat(ex1 ex2 ex3): create and done

--- ex-2.js
+++ ex-2.js
@@ -9,4 +9,43 @@ let isPlatinum;
 
 let hasPromotion;
 
-console.log(hasPromotion);
+
+function levelofCustomer(lastMonthPaid, dayBought, boughtProductFromITCategory, discountHasattend, memberLevel){
+    return {
+        lastMonthPaid: lastMonthPaid, 
+        dayBought: dayBought, 
+        boughtProductFromITCategory: boughtProductFromITCategory, 
+        discountHasattend: discountHasattend, 
+        memberLevel: memberLevel
+    };
+}
+
+
+const weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+function checkWeekday(dayinput){
+    return weekdays.includes(dayinput);
+}
+
+//assign John
+let John = levelofCustomer(4001,'Friday', false, true, 'gold');
+
+//check for John
+let person = John
+lastMonthPaidMoreThan4000 = person['lastMonthPaid'] > 4000;
+isWeekday = checkWeekday(person['dayBought']);
+hasBoughtProductFromITCategory = person['boughtProductFromITCategory'] === true;
+hasAttendedDiscountEvent = person['discountHasattend'] === true;
+isPlatinum = person['memberLevel'] === 'platinum';
+
+if ((lastMonthPaidMoreThan4000 && isWeekday && !hasBoughtProductFromITCategory && !hasAttendedDiscountEvent) || isPlatinum) {
+    hasPromotion = true;
+    console.log('John get discount promotion.');
+}else{
+    hasPromotion = false;
+    console.log('John cannot get discount promotion.');
+}
+
+//console.log(John);
+//console.log(isWeekday);
+//console.log(isPlatinum);
+
